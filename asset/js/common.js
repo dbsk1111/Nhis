158e764772948aaa67910fe319168c57c53cde3e
dbsk1111 <dbsk1111@gmail.com>
2021-05-31 12:57:26 +0900

    210531

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,6 +1,59 @@
 $(function(){
-  $('.selet_box').click(function(){
+  // gnb submenu 노출
+  $('#gnb_menu li').mouseenter(function(){
+    $('header, .header_bottom').addClass('on')
+    $('.search_wrap').removeClass('on')
+  })
+  $('.header_bottom').mouseleave(function(){
+    $('header, .header_bottom').removeClass('on')
+    if( search_bar_open > 0){
+      $('.search_wrap').addClass('on')
+    }
+  })
 
-    $(this).prev().toggleClass('on')
+  // 검색창 노출
+  let search_bar_open = 0;
+  $('.search_button').click(function(){
+
+    if( search_bar_open == 0 ){
+      $(this).addClass('on')
+      $('.search_wrap').addClass('on')
+      search_bar_open = 1;
+    }else{
+      $(this).removeClass('on')
+      $('.search_wrap').removeClass('on')
+      search_bar_open = 0
+    }
+    // $('.search_wrap').toggleClass('on')
+    $('.header_bottom').removeClass('on')
+  })
+
+  $('.all_menu_button').click(function(){
+    $('.all_menu').toggleClass('on')
+    $('body').toggleClass('on')
+  })
+
+
+  // 드롭다운 얼로우 클릭 시 윈도우 노출
+  $('.dropdown_allow').click(function(){
+    if( $(this).hasClass('lnb_window')){
+      if( $(this).hasClass('on') ){
+        $(this).removeClass('on')
+        $(this).prev().removeClass('on')
+      }else{
+        $('.lnb_window').removeClass('on')
+        $('.lnb_window').prev().removeClass('on')
+        $(this).addClass('on')
+        $(this).prev().addClass('on')
+      }
+    }else{
+      $(this).toggleClass('on')
+      $(this).prev().toggleClass('on')
+    }
+  })
+
+  // 탑으로 버튼
+  $('.back_to_top').click(function(){
+    $(window).scrollTop(0)
   })
 })
