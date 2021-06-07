8a13212d03d5ad98b007c8b29c8c2d9cc53bce02
dbsk1111 <dbsk1111@gmail.com>
2021-06-07 17:51:37 +0900

    210607

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,27 +3,15 @@ $(function(){
   // span 클래스 명으로 배경 이미지 가져오기
   $('.visitor_menu_bottom span').each(function(){
     let imageName = $(this).attr('class');
-    $(this).css('background-image','url(./asset/img/personal/ico-'+imageName+'.png)');
+    $(this).css('background-image','url(./asset/img/visitor_menu/ico-'+imageName+'.png)');
   })
 
   // 마우스 호버로 이미지 경로 교체
-  $('.visitor_menu_personal span').hover(function(){
-    $(this).css('background-image',urlChange($(this),0))
+  $('.visitor_menu_bottom span').hover(function(){
+    let imageName = $(this).attr('class');
+    $(this).css('background-image','url(./asset/img/visitor_menu/ico-'+imageName+'-on.png)');
   },function(){
-    $(this).css('background-image',urlChange($(this),1))
+    let imageName = $(this).attr('class');
+    $(this).css('background-image','url(./asset/img/visitor_menu/ico-'+imageName+'.png)');
   })
-
-  function urlChange(i, j){
-    let thisUrl = i.css('background-image');
-    let thisUrlLeng = i.css('background-image').length - 6;
-    let addText = '-on';
-    let hoverChangeUrl;
-
-    if( j == 0){
-      hoverChangeUrl = thisUrl.slice(0, thisUrlLeng) + addText + thisUrl.slice(thisUrlLeng);
-    }else{
-      hoverChangeUrl = thisUrl.slice(0, thisUrlLeng - addText.length ) + thisUrl.slice(thisUrlLeng);
-    }
-    return hoverChangeUrl;
-  }
 })
